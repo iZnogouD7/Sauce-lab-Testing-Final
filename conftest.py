fc46ad8e6b15e1a0edbac68a28a1fc999528c051
Roshan Khadka <138598139+iZnogouD7@users.noreply.github.com>
2025-07-21 08:56:20 +0545

    Add files via upload

---
+++ conftest.py
@@ -0,0 +1,14 @@
+import pytest
+from selenium import webdriver
+
+
+@pytest.fixture()
+def driver():
+    base_url = "https://www.saucedemo.com/"
+    driver=webdriver.Firefox()
+    #driver = webdriver.ChromiumEdge()
+    driver.maximize_window()
+    driver.get(base_url)
+    yield driver
+    driver.quit()
+
